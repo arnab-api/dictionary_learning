0b651c8f24849f4698b0b8a65e8e1816586b0b1f
Adam Karvonen <adam.karvonen@gmail.com>
2024-09-03 19:18:39 +0000

    Format with ruff

--- buffer.py
+++ buffer.py
@@ -6,9 +6,9 @@ from tqdm import tqdm
 from .config import DEBUG
 
 if DEBUG:
-    tracer_kwargs = {'scan' : True, 'validate' : True}
+    tracer_kwargs = {"scan": True, "validate": True}
 else:
-    tracer_kwargs = {'scan' : False, 'validate' : False}
+    tracer_kwargs = {"scan": False, "validate": False}
 
 
 class ActivationBuffer:
@@ -16,25 +16,26 @@ class ActivationBuffer:
     Implements a buffer of activations. The buffer stores activations from a model,
     yields them in batches, and refreshes them when the buffer is less than half full.
     """
-    def __init__(self, 
-                 data, # generator which yields text data
-                 model : LanguageModel, # LanguageModel from which to extract activations
-                 submodule, # submodule of the model from which to extract activations
-                 d_submodule=None, # submodule dimension; if None, try to detect automatically
-                 io='out', # can be 'in' or 'out'; whether to extract input or output activations
-                 n_ctxs=3e4, # approximate number of contexts to store in the buffer
-                 ctx_len=128, # length of each context
-                 refresh_batch_size=512, # size of batches in which to process the data when adding to buffer
-                 out_batch_size=8192, # size of batches in which to yield activations
-                 device='cpu' # device on which to store the activations
-                 ):
-        
-        if io not in ['in', 'out']:
+
+    def __init__(
+        self,
+        data,  # generator which yields text data
+        model: LanguageModel,  # LanguageModel from which to extract activations
+        submodule,  # submodule of the model from which to extract activations
+        d_submodule=None,  # submodule dimension; if None, try to detect automatically
+        io="out",  # can be 'in' or 'out'; whether to extract input or output activations
+        n_ctxs=3e4,  # approximate number of contexts to store in the buffer
+        ctx_len=128,  # length of each context
+        refresh_batch_size=512,  # size of batches in which to process the data when adding to buffer
+        out_batch_size=8192,  # size of batches in which to yield activations
+        device="cpu",  # device on which to store the activations
+    ):
+        if io not in ["in", "out"]:
             raise ValueError("io must be either 'in' or 'out'")
 
         if d_submodule is None:
             try:
-                if io == 'in':
+                if io == "in":
                     d_submodule = submodule.in_features
                 else:
                     d_submodule = submodule.out_features
@@ -54,7 +55,7 @@ class ActivationBuffer:
         self.refresh_batch_size = refresh_batch_size
         self.out_batch_size = out_batch_size
         self.device = device
-    
+
     def __iter__(self):
         return self
 
@@ -69,10 +70,10 @@ class ActivationBuffer:
 
             # return a batch
             unreads = (~self.read).nonzero().squeeze()
-            idxs = unreads[t.randperm(len(unreads), device=unreads.device)[:self.out_batch_size]]
+            idxs = unreads[t.randperm(len(unreads), device=unreads.device)[: self.out_batch_size]]
             self.read[idxs] = True
             return self.activations[idxs]
-    
+
     def text_batch(self, batch_size=None):
         """
         Return a list of text
@@ -80,23 +81,17 @@ class ActivationBuffer:
         if batch_size is None:
             batch_size = self.refresh_batch_size
         try:
-            return [
-                next(self.data) for _ in range(batch_size)
-            ]
+            return [next(self.data) for _ in range(batch_size)]
         except StopIteration:
             raise StopIteration("End of data stream reached")
-    
+
     def tokenized_batch(self, batch_size=None):
         """
         Return a batch of tokenized inputs.
         """
         texts = self.text_batch(batch_size=batch_size)
         return self.model.tokenizer(
-            texts,
-            return_tensors='pt',
-            max_length=self.ctx_len,
-            padding=True,
-            truncation=True
+            texts, return_tensors="pt", max_length=self.ctx_len, padding=True, truncation=True
         )
 
     def refresh(self):
@@ -148,13 +143,13 @@ class ActivationBuffer:
     @property
     def config(self):
         return {
-            'd_submodule' : self.d_submodule,
-            'io' : self.io,
-            'n_ctxs' : self.n_ctxs,
-            'ctx_len' : self.ctx_len,
-            'refresh_batch_size' : self.refresh_batch_size,
-            'out_batch_size' : self.out_batch_size,
-            'device' : self.device
+            "d_submodule": self.d_submodule,
+            "io": self.io,
+            "n_ctxs": self.n_ctxs,
+            "ctx_len": self.ctx_len,
+            "refresh_batch_size": self.refresh_batch_size,
+            "out_batch_size": self.out_batch_size,
+            "device": self.device,
         }
 
     def close(self):
@@ -166,28 +161,29 @@ class ActivationBuffer:
 
 class HeadActivationBuffer:
     """
-    This is specifically designed for training SAEs for individual attn heads in Llama3. 
+    This is specifically designed for training SAEs for individual attn heads in Llama3.
     Much redundant code; can eventually be merged to ActivationBuffer.
     Implements a buffer of activations. The buffer stores activations from a model,
     yields them in batches, and refreshes them when the buffer is less than half full.
     """
-    def __init__(self, 
-                 data, # generator which yields text data
-                 model : LanguageModel, # LanguageModel from which to extract activations
-                 layer, # submodule of the model from which to extract activations
-                 n_ctxs=3e4, # approximate number of contexts to store in the buffer
-                 ctx_len=128, # length of each context
-                 refresh_batch_size=512, # size of batches in which to process the data when adding to buffer
-                 out_batch_size=8192, # size of batches in which to yield activations
-                 device='cpu', # device on which to store the activations
-                 apply_W_O = False,
-                 remote = False,
-                 ):
-        
+
+    def __init__(
+        self,
+        data,  # generator which yields text data
+        model: LanguageModel,  # LanguageModel from which to extract activations
+        layer,  # submodule of the model from which to extract activations
+        n_ctxs=3e4,  # approximate number of contexts to store in the buffer
+        ctx_len=128,  # length of each context
+        refresh_batch_size=512,  # size of batches in which to process the data when adding to buffer
+        out_batch_size=8192,  # size of batches in which to yield activations
+        device="cpu",  # device on which to store the activations
+        apply_W_O=False,
+        remote=False,
+    ):
         self.layer = layer
         self.n_heads = model.config.num_attention_heads
-        self.resid_dim = model.config.hidden_size 
-        self.head_dim = self.resid_dim //self.n_heads
+        self.resid_dim = model.config.hidden_size
+        self.head_dim = self.resid_dim // self.n_heads
         self.data = data
         self.model = model
         self.n_ctxs = n_ctxs
@@ -198,9 +194,11 @@ class HeadActivationBuffer:
         self.apply_W_O = apply_W_O
         self.remote = remote
 
-        self.activations = t.empty(0, self.n_heads, self.head_dim, device=device) # [seq-pos, n_layers, n_head, head_dim]
+        self.activations = t.empty(
+            0, self.n_heads, self.head_dim, device=device
+        )  # [seq-pos, n_layers, n_head, head_dim]
         self.read = t.zeros(0).bool()
-    
+
     def __iter__(self):
         return self
 
@@ -215,10 +213,10 @@ class HeadActivationBuffer:
 
             # return a batch
             unreads = (~self.read).nonzero().squeeze()
-            idxs = unreads[t.randperm(len(unreads), device=unreads.device)[:self.out_batch_size]]
+            idxs = unreads[t.randperm(len(unreads), device=unreads.device)[: self.out_batch_size]]
             self.read[idxs] = True
             return self.activations[idxs]
-    
+
     def text_batch(self, batch_size=None):
         """
         Return a list of text
@@ -226,23 +224,17 @@ class HeadActivationBuffer:
         if batch_size is None:
             batch_size = self.refresh_batch_size
         try:
-            return [
-                next(self.data) for _ in range(batch_size)
-            ]
+            return [next(self.data) for _ in range(batch_size)]
         except StopIteration:
             raise StopIteration("End of data stream reached")
-    
+
     def tokenized_batch(self, batch_size=None):
         """
         Return a batch of tokenized inputs.
         """
         texts = self.text_batch(batch_size=batch_size)
         return self.model.tokenizer(
-            texts,
-            return_tensors='pt',
-            max_length=self.ctx_len,
-            padding=True,
-            truncation=True
+            texts, return_tensors="pt", max_length=self.ctx_len, padding=True, truncation=True
         )
 
     def refresh(self):
@@ -250,28 +242,46 @@ class HeadActivationBuffer:
 
         while len(self.activations) < self.n_ctxs * self.ctx_len:
             with t.no_grad():
-                with self.model.trace(self.text_batch(), **tracer_kwargs, invoker_args={'truncation': True, 'max_length': self.ctx_len}, remote=self.remote):
+                with self.model.trace(
+                    self.text_batch(),
+                    **tracer_kwargs,
+                    invoker_args={"truncation": True, "max_length": self.ctx_len},
+                    remote=self.remote,
+                ):
                     input = self.model.input.save()
-                    hidden_states = self.model.model.layers[self.layer].self_attn.o_proj.input[0][0]#.save()
+                    hidden_states = self.model.model.layers[self.layer].self_attn.o_proj.input[0][
+                        0
+                    ]  # .save()
                     if isinstance(hidden_states, tuple):
                         hidden_states = hidden_states[0]
 
                     # Reshape by head
-                    new_shape = hidden_states.size()[:-1] + (self.n_heads, self.head_dim) # (batch_size, seq_len, n_heads, head_dim)
+                    new_shape = hidden_states.size()[:-1] + (
+                        self.n_heads,
+                        self.head_dim,
+                    )  # (batch_size, seq_len, n_heads, head_dim)
                     hidden_states = hidden_states.view(*new_shape)
 
                     # Optionally map from head dim to resid dim
                     if self.apply_W_O:
-                        hidden_states_W_O_shape = hidden_states.size()[:-1] + (self.model.config.hidden_size,) # (batch_size, seq_len, n_heads, resid_dim)
-                        hidden_states_W_O = t.zeros(hidden_states_W_O_shape, device=hidden_states.device)
-                        for h in range (self.n_heads):
-                            start = h*self.head_dim
-                            end = (h+1)*self.head_dim
+                        hidden_states_W_O_shape = hidden_states.size()[:-1] + (
+                            self.model.config.hidden_size,
+                        )  # (batch_size, seq_len, n_heads, resid_dim)
+                        hidden_states_W_O = t.zeros(
+                            hidden_states_W_O_shape, device=hidden_states.device
+                        )
+                        for h in range(self.n_heads):
+                            start = h * self.head_dim
+                            end = (h + 1) * self.head_dim
                             hidden_states_W_O[..., h, start:end] = hidden_states[..., h, :]
-                        hidden_states = self.model.model.layers[self.layer].self_attn.o_proj(hidden_states_W_O).save()
+                        hidden_states = (
+                            self.model.model.layers[self.layer]
+                            .self_attn.o_proj(hidden_states_W_O)
+                            .save()
+                        )
 
             # Apply attention mask
-            attn_mask = input.value[1]['attention_mask']
+            attn_mask = input.value[1]["attention_mask"]
             hidden_states = hidden_states[attn_mask != 0]
 
             # Save results
@@ -281,12 +291,12 @@ class HeadActivationBuffer:
     @property
     def config(self):
         return {
-            'layer': self.layer,
-            'n_ctxs' : self.n_ctxs,
-            'ctx_len' : self.ctx_len,
-            'refresh_batch_size' : self.refresh_batch_size,
-            'out_batch_size' : self.out_batch_size,
-            'device' : self.device
+            "layer": self.layer,
+            "n_ctxs": self.n_ctxs,
+            "ctx_len": self.ctx_len,
+            "refresh_batch_size": self.refresh_batch_size,
+            "out_batch_size": self.out_batch_size,
+            "device": self.device,
         }
 
     def close(self):
@@ -315,7 +325,6 @@ class NNsightActivationBuffer:
         out_batch_size=8192,  # size of batches in which to yield activations
         device="cpu",  # device on which to store the activations
     ):
-
         if io not in ["in", "out", "in_and_out"]:
             raise ValueError("io must be either 'in' or 'out' or 'in_and_out'")
 
@@ -327,7 +336,7 @@ class NNsightActivationBuffer:
                     d_submodule = submodule.out_features
             except:
                 raise ValueError("d_submodule cannot be inferred and must be specified directly")
-        
+
         if io in ["in", "out"]:
             self.activations = t.empty(0, d_submodule, device=device)
         elif io == "in_and_out":
@@ -364,7 +373,6 @@ class NNsightActivationBuffer:
             self.read[idxs] = True
             return self.activations[idxs]
 
-
     def tokenized_batch(self, batch_size=None):
         """
         Return a batch of tokenized inputs.
@@ -384,7 +392,7 @@ class NNsightActivationBuffer:
             return t.tensor([next(self.data) for _ in range(batch_size)], device=self.device)
         except StopIteration:
             raise StopIteration("End of data stream reached")
-        
+
     def text_batch(self, batch_size=None):
         """
         Return a list of text
@@ -409,7 +417,6 @@ class NNsightActivationBuffer:
         self.activations = self.activations[~self.read]
 
         while len(self.activations) < self.n_ctxs * self.ctx_len:
-
             with t.no_grad(), self.model.trace(
                 self.token_batch(),
                 **tracer_kwargs,
